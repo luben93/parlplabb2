9360b4244d8313b060a18af8a15a4fbdbbf32f95
lucas persson <luben93@gmail.com>
2016-03-09 15:00:33 +0100

    base layout done, no controller no model

--- src/labb2/Main.java
+++ src/labb2/Main.java
@@ -2,43 +2,124 @@ package labb2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import labb2.view.Controller;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
-    private Controller c =null;
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    private GridPane rigth;
+//    private Controller c =null;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("view/sample.fxml"));
-        Parent root = loader.load();
-//        Parent root = loader.load(getClass().getResource("view/sample.fxml"));
-//        Parent root =  FXMLLoader.load(getClass().getResource("view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("VectorDrawer");
 
-        c = loader.getController();
-//        System.out.println();
-        c.setMainApp(this);
-        System.out.println(c);
-        c.setLabel("yo");
+        initRootLayout();
+
+        showDrawArea();
+        showToolArea();
+        showAtributesArea();
+    }
+
+    private void showToolArea() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/tools.fxml"));
+            GridPane tools = (GridPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rigth.add(tools,0,0);
+            // Give the controller access to the main app.
+            Controller controller = loader.getController();
+            controller.setMainApp(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public void butt(){
-        c.setLabel("bajs");
+    private void showAtributesArea() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/attributes.fxml"));
+            GridPane atributes = (GridPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rigth.add(new Separator(Orientation.HORIZONTAL),0,1);
+            rigth.add(atributes,0,2);
+            // Give the controller access to the main app.
+            Controller controller = loader.getController();
+            controller.setMainApp(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
+    private void showDrawArea() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/sample.fxml"));
+            GridPane personOverview = (GridPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
+
+            // Give the controller access to the main app.
+            Controller controller = loader.getController();
+            controller.setMainApp(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/root.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            rigth=new GridPane();
+            rootLayout.setRight(rigth);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+//    public void butt(){
+//        c.setLabel("bajs");
+//    }
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    public Main(){
+    public Main() {
 
     }
 }
--- src/labb2/view/Controller.java
+++ src/labb2/view/Controller.java
@@ -1,39 +1,24 @@
 package labb2.view;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import labb2.Main;
 
 public class Controller {
-    @FXML
-    private Label hej;
-    @FXML
-    private Button buttButton;
 
 
     private Main main;
 
     @FXML
     private void initialize() {
-//        hej = new Label();// TODO note to self, dont CREATE @FXMLs in controller
-        hej.setText("hejsan");
-        System.out.println("init");
+
     }
 
 
-    @FXML
-    private void button() {
-        main.butt();
-    }
 
     public void setMainApp(Main main) {
         this.main = main;
-        hej.setText("halloj");
+
     }
 
-    public void setLabel(String s) {
-        System.out.println(s);
-        hej.setText(s);
-    }
+
 }
